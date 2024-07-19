bd73902901501107f815fc22c26fef41afe3d82a
Mut To <>
2024-07-19 17:00:35 +0700

    add nav

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Nav from '@/components/Nav'
+import MobileNav from '@/components/MobileNav';
 
 const HomePage = () => {
+
+  const [nav, setNav] = useState(false);
+  const openNav = () => setNav(true);
+  const closeNav = () => setNav(false);
+
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className='overflow-x-hidden'>
+      <div >
+        <MobileNav nav={nav} closeNav={closeNav}/>
+        <Nav openNav={openNav}/>
+      </div>
     
     </div>
   )
